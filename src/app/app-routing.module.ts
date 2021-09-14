47d515df32bed92eaebe0affdf34f97ef9f6b351
JacobBN <jacob.b.norgaard@gmail.com>
2021-09-14 07:30:36 -0600

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+// Route order matters (it's a first-match wins strategy). More specific routes
+// should be places above less specific routes.
+const routes: Routes = [
+  // {path: '', component: HomeComponent},
+  {path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+// Materials
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// Custom components
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    // Materials
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
