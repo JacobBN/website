77562319096f37e7edefbde36cd791d4ec569366
JacobBN <jacob.b.norgaard@gmail.com>
2021-09-18 10:28:21 -0600

    tabs added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 // Custom components
@@ -37,6 +38,7 @@ import { ProjectsComponent } from './views/projects/projects.component';
     MatButtonModule,
     MatGridListModule,
     MatIconModule,
+    MatTabsModule,
     MatToolbarModule
   ],
   providers: [],
