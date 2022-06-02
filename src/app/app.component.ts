b924822d62ed499bafc118cbb0b8eb96b03ef830
JacobBN <jacob.b.norgaard@gmail.com>
2022-06-01 22:42:47 -0600

    added custom style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'website';
--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -4,7 +4,7 @@ import { getProjects } from '../../data/projects';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
   projects = getProjects();
